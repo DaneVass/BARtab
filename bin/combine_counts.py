0b4b3afb08978e8597ef2576e0c4bbd24148e5b9
Henrietta Holze <henrietta.holze@petermac.org>
2023-07-06 14:17:54 +1000

    code formatting

--- bin/combine_counts.py
+++ bin/combine_counts.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 import sys
 
-files = sys.argv[1:len(sys.argv)-1]
+files = sys.argv[1 : len(sys.argv) - 1]
 outfile = sys.argv[-1]
 
 print("reading count tables")
@@ -14,7 +14,9 @@ for count_file in files:
     # sample name is everything before last _
     sample_name = "_".join(os.path.basename(count_file).split("_")[:-1])
     # should be faster if merging on index
-    samples.append(pd.read_csv(count_file, sep="\t", names=[sample_name], index_col=[0]))
+    samples.append(
+        pd.read_csv(count_file, sep="\t", names=[sample_name], index_col=[0])
+    )
 
 print("merging count tables")
 
@@ -22,7 +24,9 @@ if len(samples) > 1:
     # in case of multiple samples, do an iterative outer join
     combined_counts = samples[0]
     for i in range(1, len(samples)):
-        combined_counts = combined_counts.merge(samples[i], how="outer", left_index=True, right_index=True)
+        combined_counts = combined_counts.merge(
+            samples[i], how="outer", left_index=True, right_index=True
+        )
     combined_counts = combined_counts.fillna(0).astype(int).reset_index(names="Barcode")
 else:
     combined_counts = samples[0].reset_index(names="Barcode")
