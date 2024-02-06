155df789cbba556ebe1acfa01efedf6671ad7c95
Henrietta Holze <henrietta.holze@petermac.org>
2024-02-06 14:48:21 +1100

    clean up python scripts and log messages

--- bin/filter_starcode_sc.py
+++ bin/filter_starcode_sc.py
@@ -18,35 +18,31 @@ print("############ Clean PCR chimerism ############\n")
 
 # read starcode output into dataframe
 starcode_output = pd.read_csv(infile, sep="\t", header=None, names=["sequence", "count"])
-print(f"Parsed {starcode_output.shape[0]} cell ID-UMI-barcode combinations")
 
-# split sequences into CB_UMI and lineage barcode
+# split sequences into CB, UMI and lineage barcode
+starcode_output["barcode"] = starcode_output["sequence"].str[cb_umi_length:]
 starcode_output['CB_UMI'] = starcode_output['sequence'].str[0:cb_umi_length]
+starcode_output["UMI"] = starcode_output["CB_UMI"].str[cb_length:cb_umi_length]
+starcode_output["CB"] = starcode_output["CB_UMI"].str[0:cb_length]
+
+print(f"Parsed {starcode_output.shape[0]} cell ID-UMI-barcode combinations ({starcode_output['CB'].unique().shape[0]} cells, {starcode_output['barcode'].unique().shape[0]} barcodes)")
 
 # only keep CB_UMI barcode combination with highest UMI count
-idx = starcode_output.groupby(['CB_UMI'])['count'].transform(max) == starcode_output['count']
+idx = starcode_output.groupby(["CB", "UMI"])['count'].transform(max) == starcode_output['count']
 starcode_output_max = starcode_output[idx]
-print(f"Removed {starcode_output.shape[0] - starcode_output_max.shape[0]} cell ID-UMI-barcode combinations with max count, kept {starcode_output_max.shape[0]}")
+print(f"Removed {starcode_output.shape[0] - starcode_output_max.shape[0]} cell ID-UMI-barcode combinations with maximum read count")
+print(f"Kept {starcode_output_max.shape[0]} ({starcode_output_max['CB'].unique().shape[0]} cells, {starcode_output_max['barcode'].unique().shape[0]} barcodes)")
 
 # remove ties by removing all duplicated CB_UMI values
-starcode_output_max_no_ties = starcode_output_max.drop_duplicates(subset=['CB_UMI'], keep=False)
-print(f"Removed {starcode_output_max.shape[0] - starcode_output_max_no_ties.shape[0]} cell ID-UMI-barcode combinations with count ties, kept {starcode_output_max_no_ties.shape[0]}")
+starcode_output_max_no_ties = starcode_output_max.drop_duplicates(subset=["CB", "UMI"], keep=False)
+print(f"Removed {starcode_output_max.shape[0] - starcode_output_max_no_ties.shape[0]} cell ID-UMI-barcode combinations with read count ties")
+print(f"Kept {starcode_output_max_no_ties.shape[0]} ({starcode_output_max_no_ties['CB'].unique().shape[0]} cells, {starcode_output_max_no_ties['barcode'].unique().shape[0]} barcodes)")
 
 # write file to pass to umi-tools count_tab
 # create read name that contains a unique index, UMI and cell barcode
 starcode_output_max_no_ties = starcode_output_max_no_ties.reset_index()
-starcode_output_max_no_ties["barcode"] = starcode_output_max_no_ties["sequence"].str[cb_umi_length:]
 # it is essential to order by barcode before umi-tools count_tab
 starcode_output_max_no_ties = starcode_output_max_no_ties.sort_values("barcode")
-starcode_output_max_no_ties["read"] = starcode_output_max_no_ties["index"].astype(str) + "_" + starcode_output_max_no_ties["CB_UMI"].str[cb_length:cb_umi_length] + "_" + starcode_output_max_no_ties["CB_UMI"].str[0:cb_length]
+starcode_output_max_no_ties["read"] = starcode_output_max_no_ties["index"].astype(str) + "_" + starcode_output_max_no_ties["UMI"] + "_" + starcode_output_max_no_ties["CB"]
 
 starcode_output_max_no_ties[["read", "barcode"]].to_csv(outfile, index=False, sep="\t", header=False)
-
-# # create columns with just cell barcode and lineage barcode, remove all other columns
-# starcode_output_max_no_ties["cell"] = starcode_output_max_no_ties["CB_UMI"].str[0:cb_length]
-# starcode_output_max_no_ties["gene"] = starcode_output_max_no_ties["sequence"].str[cb_umi_length:]
-# starcode_output_max_no_ties = starcode_output_max_no_ties[["cell", "gene"]]
-
-# starcode_counts = starcode_output_max_no_ties.value_counts().reset_index()
-
-# starcode_counts[["gene", "cell", "count"]].to_csv(outfile, index=False, sep="\t")
